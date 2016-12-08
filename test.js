bd38a7b238eec074cff843ed10bff0297f087808
Sindre Sorhus <sindresorhus@gmail.com>
2016-12-08 23:27:43 +0100

    Update dependencies

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
+import fs from 'fs';
 import test from 'ava';
 import tempWrite from 'temp-write';
-import pathExists from 'path-exists';
 
 global.Promise = Promise;
 const execa = require('execa');
@@ -8,5 +8,5 @@ const execa = require('execa');
 test(async t => {
 	const filename = tempWrite.sync('foo');
 	await execa('./cli.js', ['--force', filename]);
-	t.false(pathExists.sync(filename));
+	t.false(fs.existsSync(filename));
 });
